2fec0e32829f69ec511f8d993853b7527caa636a
Jason Beres <jasonberes@gmail.com>
2022-04-06 14:21:02 -0500

    Team collaboration initial version

---
+++ src/app/customers/customers.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { IgxGridModule } from 'igniteui-angular';
+import { CustomersComponent } from './customers.component';
+
+describe('CustomersComponent', () => {
+  let component: CustomersComponent;
+  let fixture: ComponentFixture<CustomersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CustomersComponent ],
+      imports: [ NoopAnimationsModule, FormsModule, IgxGridModule ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CustomersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
